78bc813f3841159ec677ce2cbb87728f391e0884
akshraj chavda <90257725+akshraj-05@users.noreply.github.com>
2022-12-28 17:34:17 +0530

    drum kit

---
+++ Drum_kit/index.js
@@ -0,0 +1,65 @@
+let Touch = document.getElementsByTagName("button");
+for (let i = 0; i < Touch.length; i++) {
+    Touch[i].addEventListener("click", function () {
+        sound(this.innerHTML);
+        animation(this.innerHTML);
+    })
+}
+
+let press = document.addEventListener("keypress", function (event) {
+    sound(event.key);
+    animation(event.key);
+})
+
+function sound(word) {
+    switch (word) {
+        case "w": {
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        }
+        case "a": {
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        }
+        case "s": {
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        }
+        case "d": {
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        }
+        case "j": {
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        }
+        case "k": {
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        }
+        case "l": {
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        }
+        default: {
+        }
+    }
+}
+
+
+function animation(word) {
+    var pressedButton = document.querySelector("." + word);
+    pressedButton.classList.add("pressed");
+
+
+    setTimeout(function () {
+        pressedButton.classList.remove("pressed");
+    }, 100);
+}
